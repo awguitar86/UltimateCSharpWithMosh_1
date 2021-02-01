245c7a5b6fcd7010842e9c5265c548da7a4a88f5
Austin Wright <austin.wright@netspi.com>
2021-02-01 12:37:33 -0700

    Started Mosh's Ulimate C# 1 Course and did the first few sections.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+           var a = 1;
+           var b = 2;
+           var c = 3;
+
+           Console.WriteLine(c > b || c > a);
         }
     }
 }
